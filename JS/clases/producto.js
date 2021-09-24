4c87267e9a52562af145a40ede66bc2ef56e5ef9
¨daniela <dalual.de@gmail.com>
2021-09-24 18:03:57 -0300

    filtros

--- JS/clases/producto.js
+++ JS/clases/producto.js
@@ -1,9 +1,10 @@
 class Producto {
 
-    constructor(nombre, precio, descripcion, img) {
+    constructor(nombre, precio, descripcion, img, category) {
         this.title = nombre;
         this.price = precio;
         this.description = descripcion;
         this.img = img;
+        this.category = category;
     }
 }
